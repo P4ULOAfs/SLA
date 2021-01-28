d1dfa4373dead4a750b1ffb0771626f2b9f3bd12
P4ULOAfs <78101234+P4ULOAfs@users.noreply.github.com>
2021-01-28 18:29:12 -0300

    Add files via upload

---
+++ R4ML4N/help.js
@@ -0,0 +1,124 @@
+const help = (prefix) => {
+	return `🔰---[ *MENU ARR157-BOT* ]---🔰
+┏━━━━━━━━━━━━━━━━━━━━┓
+┃ *⚠️ NAO SPAM! NAO CALL!!! ⚠️*
+┃   *caso aocontrario  BLOCK*
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃⊱❥ PROPRIETÁRIO : *𝕭𝖔𝖓𝖋𝖆𝖓𝖙𝖊(Paulo:)*
+┃⊱❥ ATIVO : *08:00-22:00 WIB*
+┃⊱❥ PREFIX : 「 ${prefix} 」
+┃⊱❥ VERSÃO : 1.1
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ 𝑻𝒆𝒏𝒕𝒂𝒏𝒈 𝑩𝑶𝑻
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ ${prefix}info (ja fala)
+┃➢ ${prefix}owner(dono)
+┃➢ ${prefix}donasi(sla)
+┃➢ ${prefix}blocklist (lista de bloqueados)
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ 𝑴𝒆𝒏𝒖 𝑺𝑰𝑴𝑷𝑳𝑬S
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ ${prefix}sticker(foto/video ate 10s)
+┃➢ ${prefix}toimg (figu em foto
+┃➢ ${prefix}ttp (sla
+┃➢ ${prefix}tts (texto pra áudio)
+┃➢ ${prefix}nulis (caderno)
+┃➢ ${prefix}quotes
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ 𝑴𝒆𝒏𝒖 𝑫𝑶𝑾𝑵𝑳𝑶𝑨𝑫
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ ${prefix}tiktod(download)
+┃➢ ${prefix}ytmp3 (download)
+┣━━━━━━━━━━━━━━━━━━━━┛
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ 𝑴𝒆𝒏𝒖 𝑮𝑨𝑩𝑼𝑻𝒁
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ ${prefix}apakah
+┃➢ ${prefix}kapankah
+┃➢ ${prefix}bisakah
+┃➢ ${prefix}hobby
+┃➢ ${prefix}watak
+┃➢ ${prefix}rate
+┃➢ ${prefix}truth
+┃➢ ${prefix}dare
+┣━━━━━━━━━━━━━━━━━━━━┛
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ 𝑴𝒆𝒏𝒖 𝑴𝑬𝑴𝑬
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ ${prefix}meme (indo)
+┃➢ ${prefix}memeindo
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ 𝑴𝒆𝒏𝒖 𝑮𝑹𝑶𝑼𝑷
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ ${prefix}admin
+┃➢ ${prefix}welcome [1/0] (bem vindo)
+┃➢ ${prefix}add (numero sem +)
+┃➢ ${prefix}kick (marcação)
+┃➢ ${prefix}promote (da adm)
+┃➢ ${prefix}demote (tira adm)
+┃➢ ${prefix}tagall (marca todos os membros)
+┃➢ ${prefix}tagall2 (marca todos os membros)
+┃➢ ${prefix}tagall3 (marca todos os membros)
+┃➢ ${prefix}linkgrup ( link do grupo)
+┃➢ ${prefix}leave (sai do grupo)
+┃➢ ${prefix}hidetag (sla)
+┃➢ ${prefix}simih [1/0] (n aconselho ativar)
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ 𝑴𝒆𝒏𝒖 𝑪𝑶𝑴𝑳𝑰 ✖[ERRO]✖
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ ${prefix}randomhentong(ERRO)
+┃➢ ${prefix}randomloli (ERRO)
+┃➢ ${prefix}nsfwblowjob (ERRO)
+┃➢ ${prefix}nsfwneko (ERRO)
+┃➢ ${prefix}nsfwtrap (ERRO)
+┣━━━━━━━━━━━━━━━━━━━━┛
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ 𝑴𝒆𝒏𝒖 𝑶𝑻𝑯𝑬𝑹
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ ${prefix}ssweb (tira print de site)
+┃➢ ${prefix}simi (ja sabe)
+┃➢ ${prefix}ocr (pega texto da foto)
+┃➢ ${prefix}wait (procura)
+┃➢ ${prefix}tiktokstalk (stalkeia)
+┃➢ ${prefix}hilih (sla)
+┃➢ ${prefix}ytstalk (stalkeia)
+┣━━━━━━━━━━━━━━━━━━━━┛
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ 𝑴𝒆𝒏𝒖 𝑴𝑨𝑲𝑬𝑹
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ ${prefix}hartatahta (sla)
+┃➢ ${prefix}pornhub (logo do ph)
+┃➢ ${prefix}thundername (logo)
+┃➢ ${prefix}galaxytext (logo)
+┃➢ ${prefix}glitchtext (logo)
+┃➢ ${prefix}partytext (logo)
+┃➢ ${prefix}bucintext (logo)
+┃➢ ${prefix}glowtext (logo)
+┃➢ ${prefix}lovetext (logo)
+┃➢ ${prefix}gugeltext (logo)
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ 𝑴𝒆𝒏𝒖 𝑶𝑾𝑵𝑬𝑹
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ ${prefix}setprefix (muda prefixo)
+┃➢ ${prefix}bc (transmissão)
+┃➢ ${prefix}clearall (limpa tudo)
+┃➢ ${prefix}clone (clona foto)
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃⟪ CRIADO POR BY *BRUXINHO MODS* ⟫
+┃⊱❥ CANAL DO BRUXINHO https://youtube.com/channel/UCalQOjX5BFqim6x8JyCMM-w
+┃⊱❥ NUMERO DO BRUXINHO http://wa.me/+27624213430
+┃⊱❥ LINK GP DO BRUXINHO https://chat.whatsapp.com/FvPEpkaPrFXAErjML3ekyW
+┣━━━━━━━━━━━━━━━━━━━━┓
+
+             ╲    ╱    ● ᏴϴͲ●ᎷᎬΝႮ●
+       ╱▔▔▔▔▔╲       Autor    : ARR157 BOT
+      ┃┈▇┈┈▇┈┃      
+╭╮┣━━━━━━┫╭╮    
+┃┃┃┈┈┈┈┈┈┃┃┃    
+╰╯┃┈┈┈┈┈┈┃╰╯
+      ╰┓┏━━┓┏╯
+         ╰╯      ╰╯
+`
+}
+
+exports.help = help
